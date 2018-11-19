73439ef4d9a78fad3f40caefcbc507b9b3e09aeb
Your Name <you@example.com>
2018-11-19 12:09:17 +0800

    hello ?

---
+++ resources/assets/js/components/BlogPosts.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="blog-section">
+        <div class="container">
+            <h1 class="text-center">From Our Blog</h1>
+
+            <p class="section-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore vitae nisi, consequuntur illum dolores cumque pariatur quis provident deleniti nesciunt officia est reprehenderit sunt aliquid possimus temporibus enim eum hic.</p>
+
+            <div class="blog-posts">
+                <div class="blog-post" id="blog1">
+                    <a href="#"><img src="img/blog1.png" > </a>
+                    <a href="#"><h2 class="blog-title">Lorem Ipsum</h2></a>
+                    <div class="blog-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore vitae nisi, consequuntur illum dolores cumque pariatur quis provident deleniti nesciunt officia est reprehenderit sunt aliquid possimus temporibus enim eum hic.</div>
+                </div>
+
+                 <div class="blog-post" id="blog2">
+                    <a href="#"><img src="img/blog2.png" > </a>
+                    <a href="#"><h2 class="blog-title">Lorem Ipsum</h2></a>
+                    <div class="blog-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore vitae nisi, consequuntur illum dolores cumque pariatur quis provident deleniti nesciunt officia est reprehenderit sunt aliquid possimus temporibus enim eum hic.</div>
+                </div>
+
+                 <div class="blog-post" id="blog3">
+                    <a href="#"><img src="img/blog3.png" > </a>
+                    <a href="#"><h2 class="blog-title">Lorem Ipsum</h2></a>
+                    <div class="blog-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore vitae nisi, consequuntur illum dolores cumque pariatur quis provident deleniti nesciunt officia est reprehenderit sunt aliquid possimus temporibus enim eum hic.</div>
+                </div>
+            </div>
+        </div> <!-- end container -->
+    </div> <!-- end blog-section -->
+</template>
+
+<script>
+    export default {
+      
+    }
+</script>
+
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer>
+    <div class="footer-content container">
+        <div class="made-with">Developed by <i class="fa fa-anchor anchor"></i> by Lexter Gangat</div>
+        {{ menu('footer', 'partials.menus.footer') }}
+    </div> <!-- end footer-content -->
+</footer>
